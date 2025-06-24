c57cabf6959eb257a7e9d40bfd0852a81fd59130
Oleg Morfiianets <1759220+olegdotcom@users.noreply.github.com>
2025-06-23 22:36:09 -0400

    Added TreeSitter context plugin.

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,38 +1,5 @@
 return {
   {
-    "nvim-treesitter/playground",
-    cmd = {
-      "TSBufEnable", "TSBufDisable", "TSBufToggle",
-      "TSHighlightCapturesUnderCursor", "TSPlaygroundToggle", "TSNodeUnderCursor",
-      "TSBufInfo",
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    branch = "master",
-    lazy = false,
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua", "markdown", "markdown_inline", "cpp", "go",
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-        playground = {
-          enable = true,
-          updatetime = 25,
-          persist_queries = false,
-        },
-      })
-    end,
-  },
-  {
     "lukas-reineke/headlines.nvim",
     dependencies = "nvim-treesitter/nvim-treesitter",
     ft = { "markdown" },
