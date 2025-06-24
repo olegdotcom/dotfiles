ebce7e683f60e3cbb3f1ec4028ff0442e9bff1f9
Oleg Morfiianets <1759220+olegdotcom@users.noreply.github.com>
2025-06-23 21:28:12 -0400

    Cleaned up key bindings. Added copy and paste key bindings.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-
 -- Window splitting.
 map("n", "<leader>-", "<cmd>split<CR>", { desc = "Horizontal split", unpack(opts) })
 map("n", "<leader>|", "<cmd>vsplit<CR>", { desc = "Vertical split", unpack(opts) })
@@ -17,18 +16,27 @@ map("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
 map("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
 map("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
 
--- Files
-map("n", "<leader>ff", require("fzf-lua").files, { desc = "Find files", unpack(opts) })
--- Buffers
-map("n", "<leader>fb", require("fzf-lua").buffers, { desc = "Find buffer", unpack(opts) })
--- Help tags
-map("n", "<leader>fh", require("fzf-lua").help_tags, { desc = "Find help", unpack(opts) })
--- Commands
-map("n", "<leader>fc", require("fzf-lua").commands, { desc = "Find commands", unpack(opts) })
+-- Jump half a screen down/up and center
+map("n", "<C-d>", "<C-d>zz", { desc = "Half screen down and center", unpack(opts) })
+map("n", "<C-u>", "<C-u>zz", { desc = "Half screen up and center", unpack(opts) })
+
+-- Find and center
+map("n", "n", "nzz", { desc = "Find next and center", unpack(opts) })
+map("n", "N", "Nzz", { desc = "Find prev and center", unpack(opts) })
+
+-- Copy and paste
+map("x", "<leader>p", [["_dP]], { desc = "Paste without losing yank", unpack(opts) })
+map({ "n", "x" }, "<leader>P", [["+p]], { desc = "Paste from system clipboard", unpack(opts) })
+
+-- FZF
+map("n", "<leader>ff", require("fzf-lua").files, { desc = "FZF files", unpack(opts) })
+map("n", "<leader>fb", require("fzf-lua").buffers, { desc = "FZF buffer", unpack(opts) })
+map("n", "<leader>fh", require("fzf-lua").help_tags, { desc = "FZF help", unpack(opts) })
+map("n", "<leader>fc", require("fzf-lua").commands, { desc = "FZF commands", unpack(opts) })
 
 -- LSP
-map("n", "<leader>d", vim.lsp.buf.definition, { desc = "Go to definition", unpack(opts) })
-map("n", "<leader>r", vim.lsp.buf.references, { desc = "Find references", unpack(opts) })
-map("n", "<leader>n", vim.lsp.buf.rename, { desc = "Rename symbol", unpack(opts) })
-map("n", "<leader>a", vim.lsp.buf.code_action, { desc = "Code action", unpack(opts) })
-map("n", "<leader>w", vim.diagnostic.open_float, { desc = "Show diagnostic message", unpack(opts) })
+map("n", "<leader>d", vim.lsp.buf.definition, { desc = "LSP definition", unpack(opts) })
+map("n", "<leader>r", vim.lsp.buf.references, { desc = "LSP references", unpack(opts) })
+map("n", "<leader>n", vim.lsp.buf.rename, { desc = "LSP rename symbol", unpack(opts) })
+map("n", "<leader>a", vim.lsp.buf.code_action, { desc = "LSP code action", unpack(opts) })
+map("n", "<leader>w", vim.diagnostic.open_float, { desc = "LSP show message", unpack(opts) })
